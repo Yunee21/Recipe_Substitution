496c3026051277cf9f112c2fd16e0869c53f12d3
bwh1270 <imbwh@cau.ac.kr>
2025-04-02 15:35:29 +0000

    for a test

--- app.py
+++ app.py
@@ -118,19 +118,18 @@ if st.button("제출"):
 
         col1, col2 = st.columns(2)
         with col1:
-            st.markdown("### 원본 레시피")
-            with st.expander("조리방법", expanded=True):
-                st.markdown(numbered_clean)
+            st.markdown("### 기존 레시피")
             with st.expander("재료", expanded=True):
                 st.dataframe(recipe_df['재료'], use_container_width=True)
+            with st.expander("조리방법", expanded=True):
+                st.markdown(numbered_clean)
 
         with col2:
             st.markdown("### 대체 레시피")
-            with st.expander("조리방법", expanded=True):
-                st.dataframe(recipe_df['조리방법'], use_container_width=True)
             with st.expander("재료", expanded=True):
                 st.dataframe(recipe_df['재료'], use_container_width=True)
-
+            with st.expander("조리방법", expanded=True):
+                st.dataframe(rec, use_container_width=True)
     else:
         # 누락 항목 파악
         missing = []
